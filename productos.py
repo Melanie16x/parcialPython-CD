1b16d221ece7547e90c3ad3b9fdf61c542602314
Acuña Melanie <melaniexiomara485@gmail.com>
2024-05-03 12:46:31 -0300

    Fin de Parcial

--- productos.py
+++ productos.py
@@ -1,3 +1,8 @@
+# Importo las librerias que vo a utilizar
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Creo el array de objetos en donde almaceno mi productos con los precios y la cantidad disponible
 productos = [
     {"nombre": "Camiseta", "precio": 20, "cantidad_disponible": 100},
     {"nombre": "Pantalón", "precio": 30, "cantidad_disponible": 80},
@@ -21,3 +26,63 @@ productos = [
     {"nombre": "Collar", "precio": 40, "cantidad_disponible": 85},
 ]
 
+
+# Defino una funcion llamada "stock" para realizar los calculos necesarios sobre los productos
+def stock(productos):
+    # Creo un bucle for para iterar sobre cada producto
+    for producto in productos:
+        # Multiplico el precio de cada producto con su cantidad disponible para hallar el valor total de 
+        # inventario de cada producto y lo imprimo por la terminal
+        valor_inventario_por_producto = producto['precio'] * producto['cantidad_disponible']
+        print(f'El valor total del inventario para el producto {producto['nombre']} es: ', valor_inventario_por_producto)
+
+    # Sumo los totales obtenidos de los inventarios por producto para sacar el total de inventario de todos 
+    # los produtos y lo imprimo por la terminal
+    valor_total_inventario = sum(producto['precio'] * producto['cantidad_disponible'] for producto in productos)
+    print('El valor total del inventario es de: ', valor_total_inventario)
+
+    # Simulo 3 ventas creando un array de tuplas
+    ventas = [("Vestido", 2), ("Pulsera", 5), ("Bolso", 7)]
+
+    # Creo dos bucles for para iterar sobre cada tupla y sobre los productos para poder restar la cantidad del
+    # producto vendido
+    for venta in ventas:
+        for producto in productos:
+            if producto["nombre"] == venta[0]:
+                producto["cantidad_disponible"] -= venta[1]
+
+    # Imprimo la cantidad disponible de cada producto luego de la simulacion de ventas
+    print("La cantidad restante disponible de cada producto despues de las ventas es: ")
+    for producto in productos:
+        print(f"{producto['nombre']}: {producto['cantidad_disponible']}")
+
+    # Creo un DataFrame con dos columnas, una para el nombre de los productos y otra para la cantidad disponible
+    dataFrame = pd.DataFrame(productos, columns=["nombre", "cantidad_disponible"])
+    print(dataFrame)
+
+    # Retorno el DataFrame para su posterior uso
+    return dataFrame
+
+# Defino una funcion para mostrar el grafico
+def grafico(productos):
+    # LLamo al DataFrame que retorno en la funcion anterior
+    dataFrame = stock(productos)
+
+    # Obtengo el nombre y la cantidad disponible de los productos
+    nombre = dataFrame['nombre']
+    cantidad_disponible = dataFrame['cantidad_disponible']
+
+    # Crear grafico
+    plt.figure(figsize=(10,6))
+    plt.plot(nombre, cantidad_disponible)
+
+    # Dar un titulo e indicar el nombre de x e y al grafico
+    plt.title('Cantidad disponible de productos')
+    plt.xlabel('nombre')
+    plt.ylabel('cantidad_disponible')
+
+    # Mostrar el grafico
+    plt.grid(True)
+    plt.show()
+
+grafico(productos)
